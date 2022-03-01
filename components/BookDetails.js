bc7d75875fc9e5e113ada682f731a066c6b287db
Ahnaf An Nafee <ahnafnafee@gmail.com>
2022-03-01 15:33:35 -0500

    refactored book details

---
+++ components/BookDetails.js
@@ -0,0 +1,169 @@
+import * as React from "react";
+import { IconButton, Image, Text } from "@chakra-ui/react";
+import Rating from "@mui/material/Rating";
+import { Menu, MenuButton, MenuList, MenuItem } from "@chakra-ui/react";
+import { EllipsisVertical, HeartOutline, AddOutline } from "react-ionicons";
+
+export function BookDetails({
+    id,
+    volumeInfo,
+    isSearch = true,
+    isLibrary = false,
+}) {
+    const extractThumbnail = ({ imageLinks }) => {
+        const DEFAULT_THUMBNAIL = "/vercel.svg";
+        if (!imageLinks || !imageLinks.thumbnail) {
+            return DEFAULT_THUMBNAIL;
+        }
+        return imageLinks.thumbnail.replace("http://", "https://");
+    };
+
+    const extractISBN13 = ({ industryIdentifiers }) => {
+        industryIdentifiers.filter((isbn) => {
+            if (isbn.type === "ISBN_13") {
+                return isbn.identifier;
+            }
+        });
+    };
+
+    return (
+        <div key={id}>
+            <div
+                style={{
+                    padding: 10,
+                    marginBottom: 16,
+                    borderRadius: 8,
+                    minHeight: 150,
+                    borderColor: "var(--chakra-colors-gray-300)",
+                    borderWidth: 1,
+                    borderStyle: "solid",
+                }}
+                className="flex flex-row justify-between"
+            >
+                <div className="flex flex-row">
+                    <Image
+                        width={"30%"}
+                        objectFit="contain"
+                        alt="Logo"
+                        src={extractThumbnail(volumeInfo)}
+                        align={"center"}
+                        borderRadius={8}
+                        marginRight={4}
+                        className="self-start"
+                    />
+                    <div className="flex flex-col justify-between">
+                        <Text fontWeight={"medium"} fontSize="lg" noOfLines={2}>
+                            {volumeInfo.title}
+                        </Text>
+                        <Text fontSize="sm" noOfLines={1}>
+                            by {volumeInfo.authors}
+                        </Text>
+                        <Text
+                            fontWeight={"light"}
+                            as="i"
+                            fontSize="sm"
+                            noOfLines={1}
+                        >
+                            {volumeInfo.categories === undefined
+                                ? "Others"
+                                : volumeInfo.categories}
+                        </Text>
+                        <Rating
+                            size="small"
+                            defaultValue={2.5}
+                            precision={0.1}
+                            value={volumeInfo.averageRating}
+                            readOnly
+                        />
+                    </div>
+                </div>
+                <div width={50} className="flex flex-col justify-between pl-2">
+                    {isSearch ? (
+                        <>
+                            <IconButton
+                                aria-label="Add to friends"
+                                icon={<HeartOutline size="35" color="#000" />}
+                                variant="outline"
+                                onClick={() =>
+                                    console.log("Wishlisted", id, volumeInfo)
+                                }
+                            />
+                            <IconButton
+                                aria-label="Add to friends"
+                                icon={<AddOutline size="35" color="#000" />}
+                                variant="outline"
+                                onClick={() =>
+                                    console.log(
+                                        "Added to Library",
+                                        id,
+                                        volumeInfo
+                                    )
+                                }
+                            />
+                        </>
+                    ) : (
+                        <>
+                            <Menu autoSelect={false}>
+                                <MenuButton
+                                    as={IconButton}
+                                    aria-label="Options"
+                                    icon={
+                                        <EllipsisVertical
+                                            size="20"
+                                            color="#000"
+                                        />
+                                    }
+                                    variant="outline"
+                                />
+                                {isLibrary ? (
+                                    <>
+                                        <MenuList>
+                                            <MenuItem
+                                                onClick={() =>
+                                                    console.log(
+                                                        "Deleted from Library",
+                                                        id,
+                                                        volumeInfo
+                                                    )
+                                                }
+                                            >
+                                                Delete
+                                            </MenuItem>
+                                        </MenuList>
+                                    </>
+                                ) : (
+                                    <>
+                                        <MenuList>
+                                            <MenuItem
+                                                onClick={() =>
+                                                    console.log(
+                                                        "Moved to Library",
+                                                        id,
+                                                        volumeInfo
+                                                    )
+                                                }
+                                            >
+                                                Move to Library
+                                            </MenuItem>
+                                            <MenuItem
+                                                onClick={() =>
+                                                    console.log(
+                                                        "Deleted from Wishlist",
+                                                        id,
+                                                        volumeInfo
+                                                    )
+                                                }
+                                            >
+                                                Delete
+                                            </MenuItem>
+                                        </MenuList>
+                                    </>
+                                )}
+                            </Menu>
+                        </>
+                    )}
+                </div>
+            </div>
+        </div>
+    );
+}
--- pages/library.js
+++ pages/library.js
@@ -1,19 +1,8 @@
 import * as React from "react";
 import Head from "next/head";
-import { IconButton, Image, Text } from "@chakra-ui/react";
-import { Avatar, AvatarBadge, AvatarGroup } from "@chakra-ui/react";
-import Rating from "@mui/material/Rating";
-import {
-    Menu,
-    MenuButton,
-    MenuList,
-    MenuItem,
-    MenuItemOption,
-    MenuGroup,
-    MenuOptionGroup,
-    MenuDivider,
-} from "@chakra-ui/react";
-import { EllipsisVertical, Trash } from "react-ionicons";
+import { Text } from "@chakra-ui/react";
+import { Avatar } from "@chakra-ui/react";
+import { BookDetails } from "../components/BookDetails";
 
 export default function Library() {
     const [books, setBooks] = React.useState([]);
@@ -25,22 +14,6 @@ export default function Library() {
             response.json().then((data) => setBooks(data.items))
         );
     }, []);
-
-    const extractThumbnail = ({ imageLinks }) => {
-        const DEFAULT_THUMBNAIL = "/vercel.svg";
-        if (!imageLinks || !imageLinks.thumbnail) {
-            return DEFAULT_THUMBNAIL;
-        }
-        return imageLinks.thumbnail.replace("http://", "https://");
-    };
-
-    const extractISBN13 = ({ industryIdentifiers }) => {
-        industryIdentifiers.filter((isbn) => {
-            if (isbn.type === "ISBN_13") {
-                return isbn.identifier;
-            }
-        });
-    };
 
     console.log(books);
 
@@ -71,80 +44,12 @@ export default function Library() {
                 >
                     {books &&
                         books.map(({ id, volumeInfo }) => (
-                            <div key={id}>
-                                <div
-                                    style={{
-                                        padding: 10,
-                                        marginBottom: 16,
-                                        borderRadius: 8,
-                                        minHeight: 150,
-                                        borderColor:
-                                            "var(--chakra-colors-gray-300)",
-                                        borderWidth: 1,
-                                        borderStyle: "solid",
-                                    }}
-                                    className="flex flex-row justify-between"
-                                >
-                                    <div className="flex flex-row">
-                                        <Image
-                                            width={"30%"}
-                                            objectFit="contain"
-                                            alt="Logo"
-                                            src={extractThumbnail(volumeInfo)}
-                                            align={"center"}
-                                            borderRadius={8}
-                                            marginRight={4}
-                                            className="self-start"
-                                        />
-                                        <div className="flex flex-col justify-between">
-                                            <Text
-                                                fontWeight={"medium"}
-                                                fontSize="lg"
-                                                noOfLines={2}
-                                            >
-                                                {volumeInfo.title}
-                                            </Text>
-                                            <Text fontSize="sm" noOfLines={1}>
-                                                by {volumeInfo.authors}
-                                            </Text>
-                                            <Text
-                                                fontWeight={"light"}
-                                                as="i"
-                                                fontSize="sm"
-                                                noOfLines={1}
-                                            >
-                                                {volumeInfo.categories ===
-                                                undefined
-                                                    ? "Others"
-                                                    : volumeInfo.categories}
-                                            </Text>
-                                            <Rating
-                                                size="small"
-                                                defaultValue={2.5}
-                                                precision={0.1}
-                                                value={volumeInfo.averageRating}
-                                                readOnly
-                                            />
-                                        </div>
-                                    </div>
-                                    <Menu autoSelect={false}>
-                                        <MenuButton
-                                            as={IconButton}
-                                            aria-label="Options"
-                                            icon={
-                                                <EllipsisVertical
-                                                    size="20"
-                                                    color="#000"
-                                                />
-                                            }
-                                            variant="outline"
-                                        />
-                                        <MenuList>
-                                            <MenuItem>Delete</MenuItem>
-                                        </MenuList>
-                                    </Menu>
-                                </div>
-                            </div>
+                            <BookDetails
+                                id={id}
+                                volumeInfo={volumeInfo}
+                                isLibrary={true}
+                                isSearch={false}
+                            />
                         ))}
                 </div>
             </div>
