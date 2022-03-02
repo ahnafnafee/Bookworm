00aa0b31277e9caa378798c5ce53c4c8866dfd69
Ahnaf An Nafee <ahnafnafee@gmail.com>
2022-03-02 02:52:58 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,5 +14,6 @@ module.exports = {
             },
         },
     },
+    purge: ["./components//*.{js,ts,jsx,tsx}", "./pages//*.{js,ts,jsx,tsx}"],
     plugins: [require("@tailwindcss/ui")],
 };
